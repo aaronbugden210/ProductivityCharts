4a8a348866d58a525a10ecbb9eb79448078fe5f5
aaronbugden210 <aaronbugden@gmail.com>
2019-10-19 18:36:11 -0400

    Can calculate averages, but for pie chart

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var pieBtn = $("#pieBtn");
 var barBtn = $("#barBtn");
 var radarBtn = $("#radarBtn");
 
+var pieAveBtn = $("#pieAveBtn");
+
+var days = 3.0;
+
 var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, 
 {
@@ -223,6 +227,63 @@ $(radarBtn).on("click", function()
     });
 });
 
+$(pieAveBtn).on("click", function()
+{
+    myChart.destroy();
+    myChart = new Chart(ctx,
+    {
+        type: 'pie',
+        data:
+        {
+            labels: ['Work', 'MTG', 'Coding', 'Eating', 'Cat', 'Drumming'],
+            datasets:
+            [{
+                label: '# of Votes',
+                data: [(parseFloat($("#workInputMon").val()) + parseFloat($("#workInputTue").val()) + parseFloat($("#workInputWed").val())) / days,
+                        (parseFloat($("#MTGInputMon").val()) + parseFloat($("#MTGInputTue").val()) + parseFloat($("#MTGInputWed").val())) / days,
+                        (parseFloat($("#codingInputMon").val()) + parseFloat($("#codingInputTue").val()) + parseFloat($("#codingInputWed").val())) / days,
+                        (parseFloat($("#eatingInputMon").val()) + parseFloat($("#eatingInputTue").val()) + parseFloat($("#eatingInputWed").val())) / days,
+                        (parseFloat($("#catInputMon").val()) + parseFloat($("#catInputTue").val()) + parseFloat($("#catInputWed").val())) / days,
+                        (parseFloat($("#drummingInputMon").val()) + parseFloat($("#drummingInputTue").val()) + parseFloat($("#drummingInputWed").val())) / days],
+                backgroundColor: 
+                [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor:
+                [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options:
+        {
+            events: ["click", "mousemove"],
+            maintainAspectRatio: false,
+            scales:
+            {
+                yAxes:
+                [{
+                    ticks:
+                    {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    
+});
 //Need to create a function to generate chart based on 'btn' press
 
 // function chartType()
